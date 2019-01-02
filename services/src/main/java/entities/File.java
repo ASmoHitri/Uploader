f174a42251823b4c05350d956187b42bb98013de
Maja Umek <mu2729@student.uni-lj.si>
2019-01-02 22:52:13 +0100

    fixing songs

---
+++ services/src/main/java/entities/File.java
@@ -0,0 +1,12 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "files")
+public class File {
+    @Id
+    private Integer songId;
+
+    private String
+}
